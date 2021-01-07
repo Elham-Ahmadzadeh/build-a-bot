b2ace0a186bd2ef6b14d8f1420d53be6e58e4174
Elham Ahmadzadeh <elhamahmadzadeh@Elhams-MacBook-Air.local>
2021-01-07 16:40:36 +0100

    RobotBuilder hasbeen updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,14 +8,20 @@ module.exports = {
     '@vue/standard'
   ],
   parserOptions: {
-    parser: 'babel-eslint'
-   
+	parser: 'babel-eslint',
+	"extends": "airbnb",
+
   },
+
   rules: {
-  
+
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    "linebreak-style": 0,
-    "comma-dangle": ["error", "never"],
-  }, 
+	"linebreak-style": 0,
+	"eol-last": 0,
+	"comma-dangle": ["error", "never"],
+	"no-tabs": 0,
+
+  },
+
 }
